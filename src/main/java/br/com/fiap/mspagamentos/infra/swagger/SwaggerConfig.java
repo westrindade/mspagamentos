c30e34f0b5fef4af3d101f3df621630eb65c0873
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-07-04 13:35:59 -0300

    Alterado para expor swagger via docker

--- src/main/java/br/com/fiap/mspagamentos/infra/swagger/SwaggerConfig.java
+++ src/main/java/br/com/fiap/mspagamentos/infra/swagger/SwaggerConfig.java
@@ -5,21 +5,48 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${spring.profiles.active:Unknown}")
+    String activeProfile;
+
+    @Value("${spring.application.name:Unknown}")
+    String applicationName;
+
+    @Value("${server.port:Unknown}")
+    String serverPort;
+
+
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
+    OpenAPI openAPI() {
+        OpenAPI securityItem = new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearer-key",new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
                         .title("APIs do Microserviço de Pagamentos")
                         .version("v1")
                         .description("APIs do Microserviço de Pgamentos criada exclusivamente para o TechChallenge 5 da FIAP.")
+                        
                 )
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+        
+        if("docker".equals(activeProfile)){
+            securityItem.servers(Arrays.asList(getServer()));
+        }
+        return securityItem;       
+    }
+
+    private Server getServer() {
+        Server server = new Server();
+        server.setDescription("Gateway Docker");
+        server.setUrl("http://"+applicationName+":"+serverPort);
+        return server;
     }
 }
