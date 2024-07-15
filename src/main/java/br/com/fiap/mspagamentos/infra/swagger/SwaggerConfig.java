f4e37b90a8462c712955c7dc533a7d2670a528ac
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-07-14 22:36:23 -0300

    remoçao server customizado swagger

--- src/main/java/br/com/fiap/mspagamentos/infra/swagger/SwaggerConfig.java
+++ src/main/java/br/com/fiap/mspagamentos/infra/swagger/SwaggerConfig.java
@@ -5,26 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    @Value("${spring.profiles.active:Unknown}")
-    String activeProfile;
-
-    @Value("${spring.application.name:Unknown}")
-    String applicationName;
-
-    @Value("${server.port:Unknown}")
-    String serverPort;
-
-
     @Bean
     OpenAPI openAPI() {
         OpenAPI securityItem = new OpenAPI()
@@ -36,17 +21,8 @@ public class SwaggerConfig {
                         
                 )
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
-        
-        if("docker".equals(activeProfile)){
-            securityItem.servers(Arrays.asList(getServer()));
-        }
+       
         return securityItem;       
     }
 
-    private Server getServer() {
-        Server server = new Server();
-        server.setDescription("Gateway Docker");
-        server.setUrl("http://"+applicationName+":"+serverPort);
-        return server;
-    }
 }
